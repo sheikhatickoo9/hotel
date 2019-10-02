3e3747892097fcbbf6d322352bb20fd98405ed08
sheikhatickoo9 <sheikhatickoo9@gmail.com>
2019-10-02 20:50:06 +0530

    @sheikha basic setup done

---
+++ Myuserapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyuserappConfig(AppConfig):
+    name = 'Myuserapp'
---
+++ Myuserapp/urls.py
@@ -0,0 +1,5 @@
+from Myuserapp import views
+from django.conf.urls import url
+urlpatterns = [
+    url(r'^home/',views.index)
+    ]
