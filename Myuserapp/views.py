e14828c7cc911ff241638a362bf8b1a2302b0bf9
sheikhatickoo9 <sheikhatickoo9@gmail.com>
2019-10-08 18:30:29 +0530

    @sheikha views.py changes

--- Myuserapp/views.py
+++ Myuserapp/views.py
@@ -3,16 +3,17 @@ from miscellaneous import emailsend
 from authorize import authcheck
 from Myuserapp.models import UserSignup
 from Myuserapp.forms import UserSignupForm
+from django.contrib.auth.hashers import make_password,check_password
 def index(request):
     return render(request, "home.html")
 
 # Create your views here.
 def usersignup(request):
     if request.method=="POST":
+        #email is object&"email" is textbox name
         email = request.POST["email"]
         form=UserSignupForm(request.POST)
         otp, time = emailsend.gen_otp()
-        confirmationlink = " http://127.0.0.1:8000/verifyuser/?email=" + email + "&token=" + otp
         f=form.save(commit=False)
         f.userFullName=request.POST["name"]
         f.userEmail = request.POST["email"]
@@ -24,10 +25,14 @@ def usersignup(request):
         f.otpcolumn=otp
         f.otptime=time
         f.roleId_id = 1
+        token =email[0:3]+request.POST['password'][0:2]+otp
+        token=make_password(token)
+        f.usertoken=token
+        confirmationlink = " http://127.0.0.1:8000/verifyuser/?email=" + email + "&token=" + token
         f.otpconfirmation=confirmationlink
         f.isActive=True
         f.isverified=False
-        f.usertoken=otp
+        #f.usertoken=otp
         f.save()
         emailsend.sendmail("link",email,confirmationlink)
         return render(request,'sign.html',{"sucess":True})
